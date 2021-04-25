8c3dfa03d18f0993495a6e332277c786aa1cfca4
Vishal_pathak <vishal.pathak@quantiphi.com>
2021-04-25 15:17:05 +0530

    regular

--- utils.py
+++ utils.py
@@ -8,10 +8,14 @@ data_f = '/home/vishal_pathak_quantiphi_com/notebooks/coleridge_data/'
 def clean_text(txt):
     return re.sub('[^A-Za-z0-9]+', ' ', str(txt).lower())
 
+def read_json(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
 def get_json(uid, split='train'):
     filepath = data_f + split + '/' + uid + '.json'
-    with open(filepath, 'r') as f:
-        return json.load(f)
+    return read_json(filepath)
     
 json_text = lambda jload: '\n\n\t'.join([i['section_title'] + ': ' + i['text'] for i in jload])
 
