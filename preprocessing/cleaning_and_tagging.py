1c25c69431f3b3e042da4abfb0b31e62d9932401
Vishal_pathak <vishal.pathak@quantiphi.com>
2021-05-30 19:00:57 +0530

    regular

--- preprocessing/cleaning_and_tagging.py
+++ preprocessing/cleaning_and_tagging.py
@@ -1,6 +1,6 @@
 import pandas as pd 
-import ..config as cfg 
-import ..utils
+import config as cfg
+import utils
 from collections import defaultdict
 from tagging_utils import *
 import time, os, re
