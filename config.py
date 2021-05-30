75243c08d028e4fa868f48ce680334ffe9abf80b
Vishal_pathak <vishal.pathak@quantiphi.com>
2021-05-30 19:09:06 +0530

    regular

--- config.py
+++ config.py
@@ -2,11 +2,26 @@ import os
 topfolder = '/content/drive/MyDrive/work/coleridge/'
 
 datafolder = f'{topfolder}data/'
+preprocessedfolder = f'{topfolder}preprocessed_data/'
 modelfolder = f'{topfolder}models/'
 outputfolder = f'{topfolder}output/'
 
-def create_subfolders():
-    for folder in [datafolder, modelfolder, outputfolder]:
-        if not os.path.exists(folder): os.makedirs(folder)
 
+def create_subfolders():
+    for folder in [datafolder, modelfolder, outputfolder, preprocessedfolder]:
+        if not os.path.exists(folder): os.makedirs(folder)
 create_subfolders()
+
+train_csvpath = f'{datafolder}train.csv'
+test_csvpath = f'{datafolder}test.csv'
+train_folder = f'{datafolder}train/'
+test_folder = f'{datafolder}test/'
+
+
+
+# prepare data
+labelcols = ['dataset_title', 'dataset_label', 'cleaned_label']
+ner_tagmap = dict(0='O', 1='B', 2='I')
+
+
+
